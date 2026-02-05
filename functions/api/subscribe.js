1991894f81d2f98e91011a5bd8fddcb3ae67bb6f
faruqaygen <76394773+faruqaygen@users.noreply.github.com>
2026-02-05 13:16:19 +0300

    Update subscribe.js

--- functions/api/subscribe.js
+++ functions/api/subscribe.js
@@ -20,11 +20,22 @@ export async function onRequestPost({ request, env }) {
       body: JSON.stringify({ email: normalizedEmail })
     });
 
-    const isDuplicate = insert.status === 409;
+  if (res.status === 409) {
+    // duplicate
+    return new Response("duplicate", { status: 409 });
+  }
+
+  if (!res.ok) return new Response("Supabase hatası", { status: 500 });
+
+  return new Response(JSON.stringify({ ok: true }), {
+    headers: { "Content-Type": "application/json" }
+  });
+    
+    /*const isDuplicate = insert.status === 409;
 
     if (!insert.ok && !isDuplicate) {
       return new Response("Supabase hatası", { status: 500 });
-    }
+    }*/
 
     // 📧 Her durumda bilgilendirme maili
     await fetch("https://api.resend.com/emails", {
